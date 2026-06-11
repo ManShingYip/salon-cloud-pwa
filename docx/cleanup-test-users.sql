f8fb276127ece2f32e13c9d1a219e7916d17c4a4
langworthmelody5112002 <240060236@stu.vtc.edu.hk>
2026-06-12 07:49:27 +0800

    cleanup: test users

--- docx/cleanup-test-users.sql
+++ docx/cleanup-test-users.sql
@@ -1,5 +1,1 @@
--- 清除殘留 test users
-DELETE FROM profiles WHERE name IN ('新員工Test', '新美容師Cindy', 'Test', 'Test2', 'TestStaff');
-
--- Verify
-SELECT id, name, role FROM profiles ORDER BY name;
+DELETE FROM profiles WHERE name IN ('新員工Test', '新美容師Cindy');
